761bb82d046185f8efde7d54312d93171d92885d
M2gargu <martingg89@educastur.es>
2024-06-18 12:31:50 +0200

    armyseeder con data hecho

--- servidor/database/seeders/ArmySeeder.php
+++ servidor/database/seeders/ArmySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Army;
+use Illuminate\Support\Str;
 
 class ArmySeeder extends Seeder
 {
@@ -13,14 +15,6 @@ class ArmySeeder extends Seeder
     private $armies=[
         #region imperium
         [
-            'name'=>'Astra Militarum',
-            // 'slug'=>'astra-militarum',
-            'description'=>'La mayor fuerza de combate coherente en la galaxia que sirve como la fuerza militar primaria del Imperio del Hombre y la primera línea de defensa de las innumerables amenazas que ponen en peligro la existencia de la raza humana en el Milenio 41.',
-            'image'=>'astraMilitarumLogo.jpg',
-            // https://drawshield.net/catalog/charges/warhammer//astra-militarum-symbol.png
-            'faction_id'=>'1'
-        ],
-        [
             'name'=>'Adepta Sororitas',
             // 'slug'=>'adepta-sororitas',
             'description'=>'Las Adepta Sororitas, coloquialmente llamadas "Hermandad", cuyo brazo militar también se conoce como Hermanas de Batalla y anteriormente como Hijas del Emperador, son una división exclusivamente femenina de la iglesia estatal del Imperio del Hombre conocida como Eclesiarquía o, más formalmente, como Adeptus Ministorum.',
@@ -29,11 +23,11 @@ class ArmySeeder extends Seeder
             'faccion_id'=>'1'
         ],
         [
-            'name'=>'Grey Knights',
-            // 'slug'=>'grey-knights',
-            'description'=>'Son un Capítulo secreto y misterioso de Marines Espaciales encargado específicamente de combatir las peligrosas entidades demoníacas de la Disformidad y todos aquellos mortales que ejercen el poder corrupto de los Dioses del Caos.',
-            'image'=>'greyKnigthsLogo.jpg',
-            // https://drawshield.net/catalog/charges/warhammer//grey-knights-symbol.png
+            'name'=>'Adeptus Custodes',
+            // 'slug'=>'adeptus-custodes',
+            'description'=>'Los Adeptus Custodes fueron los primeros y más grandes de los supersoldados diseñados por el Emperador. Cada uno es un guerrero de poder superlativo y resistencia sobrehumana, un estratega y táctico que rivaliza con los más grandes generales y muchas tropas más. Son ejemplares de leyenda casi inmortales, que no se detienen ante nada para defender al Emperador y su mundo trono.',
+            'image'=>'adeptusCustodesLogo.jpg',
+            // https://static.wikia.nocookie.net/eswarhammer40k/images/6/61/Emblema_Adeptus_Custodes_7%C2%AA_Edici%C3%B3n.jpg/revision/latest?cb=20170324185133
             'faction_id'=>'1'
         ],
         [
@@ -53,11 +47,20 @@ class ArmySeeder extends Seeder
             'faction_id'=>'1'
         ],
         [
-            'name'=>'Adeptus Custodes',
-            // 'slug'=>'adeptus-custodes',
-            'description'=>'Los Adeptus Custodes fueron los primeros y más grandes de los supersoldados diseñados por el Emperador. Cada uno es un guerrero de poder superlativo y resistencia sobrehumana, un estratega y táctico que rivaliza con los más grandes generales y muchas tropas más. Son ejemplares de leyenda casi inmortales, que no se detienen ante nada para defender al Emperador y su mundo trono.',
-            'image'=>'adeptusCustodesLogo.jpg',
-            // https://static.wikia.nocookie.net/eswarhammer40k/images/6/61/Emblema_Adeptus_Custodes_7%C2%AA_Edici%C3%B3n.jpg/revision/latest?cb=20170324185133
+            'name'=>'Astra Militarum',
+            // 'slug'=>'astra-militarum',
+            'description'=>'La mayor fuerza de combate coherente en la galaxia que sirve como la fuerza militar primaria del Imperio del Hombre y la primera línea de defensa de las innumerables amenazas que ponen en peligro la existencia de la raza humana en el Milenio 41.',
+            'image'=>'astraMilitarumLogo.jpg',
+            // https://drawshield.net/catalog/charges/warhammer//astra-militarum-symbol.png
+            'faction_id'=>'1'
+        ],
+        
+        [
+            'name'=>'Grey Knights',
+            // 'slug'=>'grey-knights',
+            'description'=>'Son un Capítulo secreto y misterioso de Marines Espaciales encargado específicamente de combatir las peligrosas entidades demoníacas de la Disformidad y todos aquellos mortales que ejercen el poder corrupto de los Dioses del Caos.',
+            'image'=>'greyKnigthsLogo.jpg',
+            // https://drawshield.net/catalog/charges/warhammer//grey-knights-symbol.png
             'faction_id'=>'1'
         ],
         [
@@ -70,22 +73,6 @@ class ArmySeeder extends Seeder
         ],
         #endregion
         #region chaos
-        [
-            'name'=>'Death Guard',
-            // 'slug'=>'death-guard',
-            'description'=>'La Guardia de la Muerte es la Legión Traidora más favorecida por Nurgle, el Dios del Caos de la plaga y la desesperación. Dedicados a propagar las espantosas infecciones del Padre de la Plaga por los mundos del Imperium, estos mórbidos Marines Espaciales son vectores de plaga vivos dotados de una resistencia grotesca.',
-            'image'=>'guardiaDeLaMuerteLogo.jpg',
-            // https://ih1.redbubble.net/image.4968566957.5087/raf,360x360,075,t,fafafa:ca443f4786.jpg
-            'faccion_id'=>'2'
-        ], 
-        [
-            'name'=>'Thousand Sons',
-            // 'slug'=>'thousand-sons',
-            'description'=>'Los Mil Hijos son una Legión Traidora de hechiceros locos que juraron lealtad a Tzeentch, el Dios del Caos de la magia y el cambio. Estos sobrenaturales Marines Espaciales gobiernan sobre séquitos de mutantes retorcidos y autómatas arcanos, lanzando complots laberínticos para provocar la destrucción del Imperium.',
-            'image'=>'milHijosLogo.jpg',
-            // https://warhammer40000.com/wp-content/uploads/2023/07/00lsq1k5mr0DF8Iy.png
-            'faction_id'=>'2'
-        ],
         [
             'name'=>'Chaos Daemons',
             // 'slug'=>'chaos-daemons',
@@ -107,13 +94,55 @@ class ArmySeeder extends Seeder
             // https://p7.hiclipart.com/preview/934/248/963/warhammer-40-000-warhammer-online-age-of-reckoning-symbol-of-chaos-chaos-space-marines-symbol.jpg
             'faction_id'=>'2'
         ],
+        [
+            'name'=>'Death Guard',
+            // 'slug'=>'death-guard',
+            'description'=>'La Guardia de la Muerte es la Legión Traidora más favorecida por Nurgle, el Dios del Caos de la plaga y la desesperación. Dedicados a propagar las espantosas infecciones del Padre de la Plaga por los mundos del Imperium, estos mórbidos Marines Espaciales son vectores de plaga vivos dotados de una resistencia grotesca.',
+            'image'=>'guardiaDeLaMuerteLogo.jpg',
+            // https://ih1.redbubble.net/image.4968566957.5087/raf,360x360,075,t,fafafa:ca443f4786.jpg
+            'faccion_id'=>'2'
+        ], 
+        [
+            'name'=>'Thousand Sons',
+            // 'slug'=>'thousand-sons',
+            'description'=>'Los Mil Hijos son una Legión Traidora de hechiceros locos que juraron lealtad a Tzeentch, el Dios del Caos de la magia y el cambio. Estos sobrenaturales Marines Espaciales gobiernan sobre séquitos de mutantes retorcidos y autómatas arcanos, lanzando complots laberínticos para provocar la destrucción del Imperium.',
+            'image'=>'milHijosLogo.jpg',
+            // https://warhammer40000.com/wp-content/uploads/2023/07/00lsq1k5mr0DF8Iy.png
+            'faction_id'=>'2'
+        ],
+        
         #endregion
         #region aeldari
+         
          [
-            'name'=>'Arlequines',
-            // 'slug'=>'arlequines',
+            'name'=>'Corsairs',
+            // 'slug'=>'corsairs',
+            'description'=>'Los Aeldari Corsairs son aquellos Asuryani Outcasts que deliberadamente han elegido ganarse la vida asaltando el comercio de otras especies inteligentes de la Vía Láctea que viajan por las estrellas, particularmente los barcos del Imperio de la Humanidad.',
+            'image'=>'corsairsLogo.jpg',
+            // https://bolterandchainsword.com/uploads/gallery/album_15844/gallery_26_15844_1900.gif
+            'faction_id'=>'3'
+         ],
+         [
+            'name'=>'Craftworlds',
+            // 'slug'=>'craftworlds',
+            'description'=>'De todos los Aeldari, los Craftworlds son los más flexibles tácticamente, y sus Guerreros Aspectos les permiten especializarse en cualquier estilo de guerra.',
+            'image'=>'craftworldsLogo.jpg',
+            // https://static.wikia.nocookie.net/warhammer40k/images/d/de/Asuryani.jpg/revision/latest?cb=20230501133347
+            'faction_id'=>'3'
+         ],
+         [
+            'name'=>'Drukhari',
+            // 'slug'=>'drukhari',
+            'description'=>'Los Drukhari (Aeldarix malum) (pronunciado en ingles Druh-KAR-ee) u "Oscuros" en el Léxico Aeldari, también conocidos por los forasteros como los Eldar Oscuros, son una estirpe Aeldari abandonada y corrupta, las contrapartes sádicas y maliciosas de los Asuryani.',
+            'image'=>'drukhariLogo.jpg',
+            // https://static.wikia.nocookie.net/warhammer40k/images/3/35/DrukhariRune.jpg/revision/latest/scale-to-width-down/250?cb=20200730184358
+            'faction_id'=>'3'
+         ],
+         [
+            'name'=>'Harlequins',
+            // 'slug'=>'harlequins',
             'description'=>'Subgrupo muy distinto de la especie Aeldari que no pertenece a ninguna de las facciones Aeldari existentes. Ellos son los guardianes de la Biblioteca Negra y sirven a la enigmática deidad Aeldari llamada Dios Riendo.',
-            'image'=>'arlequines.jpg',
+            'image'=>'harlequinsLogo.jpg',
             // https://drawshield.net/catalog/charges/warhammer//harlequins-symbol.png
             'faction_id'=>'3'
          ],
@@ -121,11 +150,51 @@ class ArmySeeder extends Seeder
         #endregion
         #region xenos
         [
-            'name'=>'Ligas de los Votann',
-            // 'slug'=>'ligas-de-los-votann',
+            'name'=>'Genestealer Cults',
+            // 'slug'=>'genestealer-cults',
+            'description'=>'Un Culto Genestealer es una sociedad secreta adoradora de xenos formada y controlada por Tyranid Genestealers que prospera en los rincones oscuros del inframundo imperial en toda la galaxia. Secretos, sigilosos y absolutamente malignos, los Genestealer Cults son cánceres que crecen invisibles en los espacios ocultos del reino de la Humanidad.',
+            'image'=>'genestealerCultsLogo.jpg',
+            // https://i.pinimg.com/474x/fd/db/2d/fddb2d06b0cd5ac3b14293e82410dcc5.jpg
+            'faction_id'=>'4'
+        ],
+        [
+            'name'=>'Kroot',
+            // 'slug'=>'kroot',
+            'description'=>'Los Kroot (Krootis aviana), también designados por el Imperio como Kroot Carnivore, son una especie xenos de humanoides salvajes que son miembros del Imperio T’au. Los Kroot evolucionaron a partir de criaturas aviares en su mundo natal de Pech.',
+            'image'=>'kroot.jpg',
+            // https://static.wikia.nocookie.net/warhammer40k/images/1/1f/Kroot2.jpg/revision/latest?cb=20100706070107
+            'faction_id'=>'4'
+        ],
+        [
+            'name'=>'Leagues of Votann',
+            // 'slug'=>'leagues-of-votann',
             'description'=>'Las Ligas de los Votann las forman mineros expertos, comerciantes pragmáticos y guerreros resistentes, que manejan tecnologías avanzadas perdidas por el Imperio. Los clones que integran la Familia pueblan las Ligas y son duros de cuerpo y mente, sus ejércitos salen del núcleo galáctico para reclamar recursos y saldar deudas por cualquier medio necesario.',
             'image'=>'votannLogo.jpg',
             // https://warhammer40000.com/wp-content/uploads/2023/07/VNXJWXxmg0WcqzRv.png
+            'faction_id'=>'4'
+        ],
+        [
+            'name'=>'Necrons',
+            // 'slug'=>'necrons',
+            'description'=>'Los Necrons son una misteriosa especie xenos de guerreros esqueléticos robóticos humanoides que han permanecido inactivos en sus tumbas de estasis durante más de 60 millones de años terrestres, aunque por fin han comenzado a despertar.',
+            'image'=>'necronsLogo.jpg',
+            // https://wh40k.lexicanum.com/mediawiki/images/c/c8/Ankh_triarch.png
+            'faction_id'=>'4'
+        ],
+        [
+            'name'=>'Orks',
+            // 'slug'=>'orks',
+            'description'=>'Los Orkos, también llamados Pieles Verdes, son una especie de humanoides bestiales y asexuales, salvajes, guerreros y de piel verde que se encuentran repartidos por toda la galaxia. Son únicos entre las especies xenos inteligentes conocidas por la humanidad porque poseen las características fisiológicas tanto de animales como de hongos.',
+            'image'=>'orksLogo.jpg',
+            // https://preview.redd.it/what-is-the-orks-icon-v0-ciqyrljvhjkb1.jpg?width=250&format=pjpg&auto=webp&s=a13353914d7587d443a899204b28f2c94165a891
+            'faction_id'=>'4'
+        ],
+        [
+            'name'=>'T’au Empire',
+            // 'slug'=>'tau-empire',
+            'description'=>'El Imperio T’au (pronunciado "TOW" como en "WOW"), también escrito Imperio Tau en registros imperiales más antiguos, es un imperio estelar xenos multiespecie en rápida expansión situado dentro del Imperio del Ultima Segmentum del Hombre, cerca de la Franja Oriental del Via Láctea. Está al alcance del Astronomican.',
+            'image'=>'tauEmpireLogo.jpg',
+            // https://static.wikia.nocookie.net/warhammer40k/images/f/f0/T%27au_Empire_Icon.png/revision/latest?cb=20230512074501
             'faction_id'=>'4'
         ],
         [
@@ -140,6 +209,15 @@ class ArmySeeder extends Seeder
         ];
     public function run(): void
     {
-        //
+        foreach ($this->armies as $army) {
+            $a = new Army();
+            $a->name = $army['name'];
+            $a->slug = Str::slug($army["name"]);
+            $a->description = $army['description'];
+            $a->image = $army['image'];
+            $a->faction_id = $army['faction_id'];
+            $a->save();
+        }
+        $this->command->info('Armies created with data success!');
     }
 }
