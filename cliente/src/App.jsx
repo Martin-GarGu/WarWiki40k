57151a5bda9a07ab3bbc6443d70a4c3c422cd6c4
M2gargu <martingg89@educastur.es>
2025-05-15 12:58:03 +0200

    Footer hecho, falta modificar estilos demas paginas

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,23 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Registro from "./components/register/Register.jsx";
-import Header from "./components/header/Header.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Registro from './components/register/Register.jsx';
+import Header from './components/header/Header.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Inicio from "./components/complements/Inicio.jsx";
-import Faction from "./components/complements/Faction.jsx";
-import Login from "./components/login/Login.jsx";
-import Squad from "./components/complements/Squad.jsx";
-import { useEffect, useRef, useState } from "react";
-import Army from "./components/complements/Army.jsx";
-import Perfil from "./components/complements/Perfil.jsx";
-import CrudAdmin from "./components/complements/CrudAdmin/CrudAdmin.jsx";
-import Favoritos from "./components/complements/Favoritos.jsx";
-import Games from "./components/complements/Games.jsx";
-import CreateGames from "./components/complements/CrudAdmin/Create/CreateGames.jsx";
+import Inicio from './components/complements/Inicio.jsx';
+import Faction from './components/complements/Faction.jsx';
+import Login from './components/login/Login.jsx';
+import Squad from './components/complements/Squad.jsx';
+import { useEffect, useRef, useState } from 'react';
+import Army from './components/complements/Army.jsx';
+import Perfil from './components/complements/Perfil.jsx';
+import CrudAdmin from './components/complements/CrudAdmin/CrudAdmin.jsx';
+import Favoritos from './components/complements/Favoritos.jsx';
+import Games from './components/complements/Games.jsx';
+import CreateGames from './components/complements/CrudAdmin/Create/CreateGames.jsx';
 import Footer from './components/footer/Footer.jsx';
 import CreateFaction from './components/complements/CrudAdmin/Create/CreateFaction.jsx';
-import CreateMenu from "./components/complements/CrudAdmin/Create/CreateMenu.jsx";
+import CreateMenu from './components/complements/CrudAdmin/Create/CreateMenu.jsx';
 import CreateArmy from './components/complements/CrudAdmin/Create/CreateArmy.jsx';
 import CreateSquad from './components/complements/CrudAdmin/Create/CreateSquad.jsx';
 import CreateSoldier from './components/complements/CrudAdmin/Create/CreateSoldier.jsx';
@@ -48,203 +48,121 @@ import SoldierAdmin from './components/complements/CrudAdmin/SoldierAdmin.jsx';
 function App() {
   const headerRef = useRef(null);
   const footerRef = useRef(null);
-  const [routeHeight, setAltura] = useState("100vh");
-
+  const [routeHeight, setRouteHeight] = useState('100vh');
+  
   useEffect(() => {
+    // Ajustar altura del contenido principal basado en la altura del header
     const headerHeight = headerRef.current ? headerRef.current.offsetHeight : 0;
-    const footerHeight = footerRef.current ? footerRef.current.offsetHeight : 0;
-    const altura = `calc(100vh - ${headerHeight}px - ${footerHeight}px)`;
+    setRouteHeight(`calc(100vh - ${headerHeight}px)`);
+    
+    // Función para ajustar tamaños cuando cambia el tamaño de la ventana
+    const handleResize = () => {
+      const headerHeight = headerRef.current ? headerRef.current.offsetHeight : 0;
+      setRouteHeight(`calc(100vh - ${headerHeight}px)`);
+    };
 
-    setAltura(altura);
+    // Configuración para el botón Back to Top
+    const scrollContainer = document.getElementById('scroll-container');
+    if (scrollContainer) {
+      // Si existe un botón Back to Top en el DOM, configuramos su comportamiento
+      const backToTopButton = document.querySelector('.back-to-top');
+      if (backToTopButton) {
+        backToTopButton.addEventListener('click', () => {
+          scrollContainer.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          });
+        });
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+    
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      // Limpieza del evento click
+      const backToTopButton = document.querySelector('.back-to-top');
+      if (backToTopButton && scrollContainer) {
+        backToTopButton.removeEventListener('click', () => {});
+      }
+    };
   }, []);
 
   return (
-    <>
-      <BrowserRouter>
-        <div id="scroll-container" className="routes-container" style={{ minHeight: routeHeight, overflowY: "auto"}}>
-          <Header ref={headerRef} className="w-100"></Header>
-          <Routes>
-            <Route path="/" element={<Inicio />} />
-            <Route
-              path="/register"
-              element={
-                <div className="d-flex justify-content-center registro">
-                  <div
-                    className="col-sm-10 col-md-8 col-lg-6 col-xl-5"
-                    style={{
-                      height: routeHeight
-                    }}
-                  >
-                    <Registro />
+    <BrowserRouter>
+      <div className="app-container d-flex flex-column min-vh-100" >
+        <Header ref={headerRef} className="header w-100" />
+        
+        <main className="main-content flex-grow-1" style={{ minHeight: 'calc(100vh - 56px)' }}>
+          <div 
+            className="content-wrapper pb-4" 
+            style={{ minHeight: routeHeight, overflowY: "auto" }}
+          >
+            <Routes>
+              <Route path="/" element={<Inicio />} />
+              <Route
+                path="/register"
+                element={
+                  <div className="d-flex justify-content-center registro">
+                    <div className="col-sm-10 col-md-8 col-lg-6 col-xl-5">
+                      <Registro />
+                    </div>
                   </div>
-                </div>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <div className="d-flex justify-content-center login">
-                  <div
-                    className="col-sm-10 col-md-8 col-lg-6 col-xl-5"
-                    style={{
-                      height: routeHeight
-                    }}
-                  >
-                    <Login />
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <div className="d-flex justify-content-center login">
+                    <div className="col-sm-10 col-md-8 col-lg-6 col-xl-5">
+                      <Login />
+                    </div>
                   </div>
-                </div>
-              }
-            />
-            <Route
-              path='/rules'
-              element={<Rules />}
-            />
-            <Route
-              path="/access-denied"
-              element={<AccessDenied />}
-            />
-            <Route
-              path='/specialRules'
-              element={<SpecialRules />}
-            />
-            <Route
-              path='/createMenu'
-              element={<CreateMenu />}
-            />
-            <Route
-              path='/eliminateMenu'
-              element={<EliminateMenu />}
-            />
-            <Route
-              path='/updateMenu'
-              element={<UpdateMenu />}
-            />
-            <Route
-              path="/:slug"
-              element={<Faction />}
-            />
-            <Route
-              path="/armies/:slug"
-              element={<Army />}
-            />
-            <Route
-              path="/squads/:slug"
-              element={<Squad />}
-            />
-            <Route
-              path="/profile"
-              element={<Perfil />}
-            />
-            <Route
-              path="/crud"
-              element={<CrudAdmin />}
-            />
-            <Route
-              path="/favorites"
-              element={<Favoritos />}
-            />
-            <Route
-              path="/games"
-              element={<Games />}
-            />
-            <Route
-              path="/games/create"
-              element={<CreateGames />}
-            />
-            <Route
-              path='/create/faction'
-              element={<CreateFaction />}
-            />
-            <Route
-              path='/create/army'
-              element={<CreateArmy />}
-            />
-            <Route
-              path='/create/squad'
-              element={<CreateSquad />}
-            />
-            <Route
-              path='/create/soldier'
-              element={<CreateSoldier />}
-            />
-            <Route
-              path='/create/weapon'
-              element={<CreateWeapon />}
-            />
-            <Route
-              path='/eliminate/faction'
-              element={<EliminateFaction />}
-            />
-            <Route
-              path='/eliminate/army'
-              element={<EliminateArmy />}
-            />
-            <Route
-              path='/eliminate/squad'
-              element={<EliminateSquad />}
-            />
-            <Route
-              path='/eliminate/soldier'
-              element={<EliminateSoldier />}
-            />
-            <Route
-              path='/update/factionMenu'
-              element={<UpdateFactionMenu />}
-            />
-            <Route
-              path='/update/armyMenu'
-              element={<UpdateArmyMenu />}
-            />
-            <Route
-              path='/update/game'
-              element={<UpdateGame />}
-            />
-            <Route
-              path='/update/squadMenu'
-              element={<UpdateSquadMenu />}
-            />
-            <Route
-              path='/update/soldierMenu'
-              element={<UpdateSoldierMenu />}
-            />
-            <Route
-              path='/update/faction'
-              element={<UpdateFaction />}
-            />
-            <Route
-              path='/update/army'
-              element={<UpdateArmy />}
-            />
-            <Route
-              path='/update/squad'
-              element={<UpdateSquad />}
-            />
-            <Route
-              path='/update/soldier'
-              element={<UpdateSoldier />}
-            />
-            <Route
-              path='/crud/factionAdmin'
-              element={<FactionAdmin/>}
-            />
-            <Route
-              path='/crud/armiesAdmin'
-              element={<ArmyAdmin/>}
-            />
-            <Route
-              path='/crud/squadAdmin'
-              element={<SquadAdmin/>}
-            />
-            <Route
-              path='/crud/soldierAdmin'
-              element={<SoldierAdmin/>}
-            />
-          </Routes>
-
-        </div>
-        {/* <Footer ref={footerRef} /> */}
-      </BrowserRouter>
-    </>
+                }
+              />
+              <Route path='/rules' element={<Rules />} />
+              <Route path="/access-denied" element={<AccessDenied />} />
+              <Route path='/specialRules' element={<SpecialRules />} />
+              <Route path='/createMenu' element={<CreateMenu />} />
+              <Route path='/eliminateMenu' element={<EliminateMenu />} />
+              <Route path='/updateMenu' element={<UpdateMenu />} />
+              <Route path="/:slug" element={<Faction />} />
+              <Route path="/armies/:slug" element={<Army />} />
+              <Route path="/squads/:slug" element={<Squad />} />
+              <Route path="/profile" element={<Perfil />} />
+              <Route path="/crud" element={<CrudAdmin />} />
+              <Route path="/favorites" element={<Favoritos />} />
+              <Route path="/games" element={<Games />} />
+              <Route path="/games/create" element={<CreateGames />} />
+              <Route path='/create/faction' element={<CreateFaction />} />
+              <Route path='/create/army' element={<CreateArmy />} />
+              <Route path='/create/squad' element={<CreateSquad />} />
+              <Route path='/create/soldier' element={<CreateSoldier />} />
+              <Route path='/create/weapon' element={<CreateWeapon />} />
+              <Route path='/eliminate/faction' element={<EliminateFaction />} />
+              <Route path='/eliminate/army' element={<EliminateArmy />} />
+              <Route path='/eliminate/squad' element={<EliminateSquad />} />
+              <Route path='/eliminate/soldier' element={<EliminateSoldier />} />
+              <Route path='/update/factionMenu' element={<UpdateFactionMenu />} />
+              <Route path='/update/armyMenu' element={<UpdateArmyMenu />} />
+              <Route path='/update/game' element={<UpdateGame />} />
+              <Route path='/update/squadMenu' element={<UpdateSquadMenu />} />
+              <Route path='/update/soldierMenu' element={<UpdateSoldierMenu />} />
+              <Route path='/update/faction' element={<UpdateFaction />} />
+              <Route path='/update/army' element={<UpdateArmy />} />
+              <Route path='/update/squad' element={<UpdateSquad />} />
+              <Route path='/update/soldier' element={<UpdateSoldier />} />
+              <Route path='/crud/factionAdmin' element={<FactionAdmin/>} />
+              <Route path='/crud/armiesAdmin' element={<ArmyAdmin/>} />
+              <Route path='/crud/squadAdmin' element={<SquadAdmin/>} />
+              <Route path='/crud/soldierAdmin' element={<SoldierAdmin/>} />
+            </Routes>
+          </div>
+        </main>
+        
+        <Footer ref={footerRef} />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- cliente/src/components/footer/Footer.jsx
+++ cliente/src/components/footer/Footer.jsx
@@ -1,15 +1,74 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Footer = React.forwardRef((props, ref) => {
-    return (
-        <footer ref={ref} className="footer row m-0">
-            <div className="col-12 text-center">
-                <a href="#scroll-container" className="footer-link">Back to top</a>
+  return (
+    <footer ref={ref} className="footer bg-dark text-white py-4 w-100">
+      <div className="container">
+        <div className="row">
+          {/* Columna 1: Logo y descripción */}
+          <div className="col-md-4 mb-3">
+            <h5 className="mb-3">WarWiki40K</h5>
+            <p className="small">
+              Tu plataforma de informacion sobre facciones, ejércitos y escuadrones en el mundo de Warhammer 40.000.
+            </p>
+          </div>
+
+          {/* Columna 2: Enlaces útiles */}
+          <div className="col-md-4 mb-3">
+            <h5 className="mb-3">Enlaces útiles</h5>
+            <ul className="list-unstyled">
+              <li className="mb-2">
+                <Link to="/" className="text-white text-decoration-none">
+                  Inicio
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link to="/rules" className="text-white text-decoration-none">
+                  Reglas
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* Columna 3: Redes sociales */}
+          <div className="col-md-4 mb-3">
+            <h5 className="mb-3">Síguenos</h5>
+            <div className="d-flex gap-3 fs-5">
+              <a href="#" className="text-white">
+                <i className="bi bi-facebook"></i>
+              </a>
+              <a href="#" className="text-white">
+                <i className="bi bi-twitter"></i>
+              </a>
+              <a href="#" className="text-white">
+                <i className="bi bi-instagram"></i>
+              </a>
+              <a href="#" className="text-white">
+                <i className="bi bi-discord"></i>
+              </a>
             </div>
-        </footer>
-    );
+          </div>
+        </div>
+
+        {/* Línea divisoria */}
+        <hr className="my-3 border-secondary" />
+
+        {/* Copyright */}
+        <div className="row">
+          <div className="col-12 text-center">
+            <p className="small mb-0">
+              © {new Date().getFullYear()} Wargame App. Todos los derechos reservados.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 });
+
 Footer.displayName = "Footer";
 
 export default Footer;
-
